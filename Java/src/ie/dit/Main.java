3440b4d022954cd11b5fe89e514d78449105b4a2
Michael <c16434974@mydit.ie>
2018-01-24 19:55:51 +0000

    Added multiplication for matricies as well as error checking

--- Java/src/ie/dit/Main.java
+++ Java/src/ie/dit/Main.java
@@ -1,5 +1,8 @@
 package ie.dit;
 
+import static ie.dit.Matrix.Add;
+import static ie.dit.Matrix.Mult;
+
 public class Main
 {
     public static void main(String[] args)
@@ -8,17 +11,22 @@ public class Main
         m1.identity();
         System.out.println(m1);
 
-        Matrix m2 = new Matrix(3, 2);
+        Matrix m2 = new Matrix(3, 3);
         m2.setElement(2, 1, 100);
         m2.setElement(1, 1, 8);
-        System.out.println(m2.getElement(2, 1));
-        System.out.println(m2);
+        System.out.println("\n" + m2.getElement(2, 1));
+        System.out.println("\n" + m2);
 
-        //Adds it to itself
-        m2.add(m1);
-        System.out.println(m2);
+        //First add function
+//        m2.add(m1);
+//        System.out.println(m2);
 
-        //m3 = m1.add(m2);
+        //Second add funciton
+        Matrix m3 = new Matrix(3, 3);
+//        m3 = Add(m1, m2);
+//        System.out.println(m3);
 
+        m3 = Mult(m1, m2);
+        System.out.println("\n" + m3);
     }
 }
--- Java/src/ie/dit/Matrix.java
+++ Java/src/ie/dit/Matrix.java
@@ -10,12 +10,12 @@ public class Matrix {
     private float[][] elements;
 
     //Accessor
-    public int getRows()
+    private int getRows()
     {
         return rows;
     }//end getRows
 
-    public int getCols()
+    private int getCols()
     {
         return cols;
     }//end getRows
@@ -34,7 +34,7 @@ public class Matrix {
     }//end getElement
 
     //Constructor
-    public Matrix(int rows, int cols)
+    Matrix(int rows, int cols)
     {
         this.rows = rows;
         this.cols = cols;
@@ -57,47 +57,105 @@ public class Matrix {
 
     public String toString()
     {
-        String ret = "";
+        StringBuilder ret = new StringBuilder();
         //Use for loop as know how many elements there are.
         for (int row = 0;row < rows ;row++ )
         {
             for (int col = 0;col < cols ;col++ )
             {
                 //If row and col are the same, set the value to be 1, otherwise 0
-                ret += elements[row][col] + "\t";
+                ret.append(elements[row][col]).append("\t");
             }//end inner for
-            ret += "\n";
+            ret.append("\n");
         }//end outer for
-        return ret;
+        return ret.toString();
     }//end toString
 
-    //Adds a matrix to itself
-    public void add(Matrix m2)
+    //Bug occures when two matrices are not of the same size!
+    public void add(Matrix m1)
     {
-        Matrix m1 = new Matrix(this.getRows(), this.getCols());
-        if(this.getRows() < rows)
+        //Checks of rows and cols are equal
+//        if(elements.length != m1.elements.length || elements[0].length != m1.elements[0].length)
+//        {
+//            System.out.println("The matrices do not have the same number of rows and cols.");
+//        }//end if
+
+        if(( getRows() != m1.getRows() ) || ( getCols() != m1.getCols() ))
         {
-            rows = this.getRows();
+            System.out.println("Matrices must have the same number of rows");
+            System.exit(0);
         }
-        if(this.getCols() < cols)
+
+        for (int row = 0;row < rows ;row++ )
         {
-            cols = this.getCols();
-        }
-        //Use for loop as know how many elements there are.
-        for (int row = 0;row < rows;row++ )
-        {
-            for (int col = 0;col < cols;col++ )
+            for (int col = 0;col < cols ;col++ )
             {
-                //Adds from matrix m1 and m2 to a new matrix
-                //if()
-                elements[row][col] += m2.elements[row][col] + m1.elements[row][col];
-                //m2.set(rows, cols, m2.elements[row][col] + m1.elements[row][col]);
+                //Since m2 is this class, all we need to do is specify m1 for the other matrix passed
+                //i.e. -> m2 = m2 + m1 is the operation we are currently doing
+               elements[row][col] = elements[row][col] + m1.elements[row][col];
             }//end inner for
         }//end outer for
+
     }
 
-//    public static Matrix add(Matrix a, Matrix b)
-//    {
-//
-//    }
+    //Public by default so removed keyword
+    static Matrix Add(Matrix a, Matrix b)
+    {
+//        //Check if rows and cols are equal
+//        int rows = a.elements.length;
+//        int cols = a.elements[0].length;
+//        if(a.elements.length != b.elements.length || a.elements[0].length != b.elements[0].length)
+//        {
+//            System.out.println("The matrices do not have the same number of rows and cols.");
+//        }//end if
+        //Create the new variable which holds the sum of the two matrices
+        int rows = a.getRows();
+        int cols = a.getCols();
+        //Check and make sure that have same no. of cols and rows for addition
+        if(( a.getRows() != b.getRows() ) || ( a.getCols() != b.getCols() ))
+        {
+            System.out.println("Matrices must have the same number of rows");
+            System.exit(0);
+        }
+
+        Matrix ans = new Matrix(rows, cols);
+
+        for (int row = 0;row < rows ;row++ )
+        {
+            for (int col = 0;col < cols ;col++ )
+            {
+                //Since m2 is this class, all we need to do is specify m1 for the other matrix passed
+                //i.e. -> m2 = m2 + m1 is the operation we are currently doing
+                ans.elements[row][col] = a.elements[row][col] + b.elements[row][col];
+            }//end inner for
+        }//end outer for
+
+        return ans;
+    }
+
+    static Matrix Mult(Matrix a, Matrix b)
+    {
+        int rows, cols;
+        //Don't need to assign separately as in multipling same no. of rows as cols
+        rows = cols = a.getRows();
+        Matrix ans = new Matrix(rows, cols);
+        if(a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        {
+            System.out.println("Rows and cols must be the same for matrix multiplication");
+            System.exit(0);
+        }
+
+        for(int row = 0; row < rows; row++)
+        {
+            for(int col = 0; col < cols; col++)
+            {
+                //In mult rows must be the same as columns
+                for(int i = 0; i < rows; i++)
+                {
+                    ans.elements[row][i] += a.elements[row][i] * b.elements[i][col];
+                }
+            }
+        }
+        return ans;
+    }
 }//end Matrix
