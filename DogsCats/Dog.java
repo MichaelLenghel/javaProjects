18433a09f88ba3055afba5b3c0de8cfc923828fb
Michael <michael.lenghel@gmail.com>
2018-02-12 21:15:48 +0000

    added polumorphism example in java

---
+++ DogsCats/Dog.java
@@ -0,0 +1,17 @@
+public class Dog extends Animal
+{
+	public Dog(String name)
+	{
+		super(name, 4);
+	}
+	
+	public void speak()
+	{
+		System.out.println(getName() + " says Woof!");
+	}	
+	
+	public void fetch()
+	{
+		System.out.println(getName() + " is fetching");
+	}
+}
